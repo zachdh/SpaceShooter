4ff0ac3799a9128a197e3b218f54571d77ad3336
Zachary Huang <zadahu@gmail.com>
2023-12-31 23:07:32 -0800

    save

--- laser.gd
+++ laser.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 @export var speed : int = 10
 @onready var kill_timer = $KillTimer
+@export var NAME = "laser"
 
 var direction := Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
@@ -18,9 +19,6 @@ func _process(delta: float) -> void:
 func set_direction(direction: Vector2):
 	self.direction = direction
 	
-func shot():
-	pass
-
 func _on_kill_timer_timeout() -> void:
 	queue_free()
 
